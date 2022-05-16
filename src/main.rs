fcd46bf99b9c1e61306908ce65e1546aad8d226a
Jacob <jbuchanan21@qub.ac.uk>
2022-05-17 00:34:05 +0100

    day_2_part_1

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,30 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let mut prev = std::i32::MAX;
-    let mut increase_count = 0;
+    let mut depth = 0;
+    let mut horizontal = 0;
 
     if let Ok(lines) = read_lines("input.txt") {
         for line in lines {
-            let cur = line.unwrap().parse::<i32>().unwrap();
-            if prev < cur {
-                increase_count += 1;
+            let line = line.unwrap();
+            let line_val = line.split_whitespace().take(2).collect::<Vec<&str>>();
+            
+            if let [cmd, amt] = line_val[..] {
+                let amt = amt.parse::<i32>().unwrap();
+
+                match cmd {
+                    "forward" => horizontal += amt,
+                    "up" => depth -= amt,
+                    "down" => depth += amt,
+                    _ => panic!("Unrecognised command!")
+                }
             }
-            prev = cur;
         }
     }
 
-    println!("{}", increase_count);
+    println!("Final Depth: {}", depth);
+    println!("Final Horizontal Position: {}", horizontal);
+    println!("Result: {}", depth * horizontal);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
