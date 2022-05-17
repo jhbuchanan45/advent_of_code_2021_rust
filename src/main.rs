296f0d4af286b88aca22cb8eb751a243e3dc32a1
Jacob <jbuchanan21@qub.ac.uk>
2022-05-17 15:19:34 +0100

    day_3_part_1

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,46 @@
+use core::panic;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let mut depth = 0;
-    let mut horizontal = 0;
-    let mut aim = 0;
+    let mut gamma = 0;
+    let mut epsilon = 0;
+    let mut count_ones = [0; 12];
+    let mut total = 0;
 
+    // count ones
     if let Ok(lines) = read_lines("input.txt") {
         for line in lines {
             let line = line.unwrap();
-            let line_val = line.split_whitespace().take(2).collect::<Vec<&str>>();
-            
-            if let [cmd, amt] = line_val[..] {
-                let amt = amt.parse::<i32>().unwrap();
 
-                match cmd {
-                    "forward" => {horizontal += amt; depth += amt * aim},
-                    "up" => aim -= amt,
-                    "down" => aim += amt,
-                    _ => panic!("Unrecognised command!")
+            for (i, bit) in line.chars().enumerate() {
+                match bit {
+                    '1' => count_ones[i] += 1,
+                    '0' => continue,
+                    _ => panic!("Character was not '0' or '1'!"),
                 }
             }
+
+            total += 1;
         }
     }
 
-    println!("Final Depth: {}", depth);
-    println!("Final Horizontal Position: {}", horizontal);
-    println!("Result: {}", depth * horizontal);
+    // get most common for each column
+    for one_count in count_ones {
+        gamma = gamma << 1;
+        epsilon = epsilon << 1;
+
+        if one_count > total / 2 {
+            gamma += 1
+        } else {
+            epsilon += 1
+        }
+    }
+
+    println!("Final Gamma: {}", gamma);
+    println!("Final Epsilon: {}", epsilon);
+    println!("Power Consumption: {}", gamma * epsilon);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
