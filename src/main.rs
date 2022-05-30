5fe28f7c3d512db36e3a7a2715a6c1fde7f8463d
Jacob Buchanan <jhabuchanan522@gmail.com>
2022-05-30 14:42:07 +0100

    day_6_part_2

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,38 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::iter::Map;
 use std::path::Path;
 
 fn main() {
     let days = 256;
-    // let mut generation_counts = HashMap::new();
-    let mut lanternfish = Vec::new();
+    let mut timer_counts: [u64; 9] = [0; 9];
 
     if let Ok(lines) = read_lines("input.txt") {
         for line in lines {
             let clocks = line.unwrap();
 
             for clock in clocks.split(',') {
-                lanternfish.push(clock.parse::<u8>().unwrap());
+                timer_counts[clock.parse::<usize>().unwrap()] += 1;
             }
         }
     }
 
     for _ in 0..days {
-        for (i, &clock) in lanternfish.clone().iter().enumerate() {
-            if clock == 0 {
-                lanternfish.push(8);
-                lanternfish[i] = 6;
+        let mut new_timers = [0; 9];
+        for (i, count) in timer_counts.iter().enumerate() {
+            if i == 0 {
+                new_timers[6] += count;
+                new_timers[8] += count;
             } else {
-                lanternfish[i] -= 1
+                new_timers[i-1] += count;
             }
         }
+
+        timer_counts = new_timers;
     }
 
-    println!("{:?}", lanternfish.len());
+    let total = timer_counts.iter().sum::<u64>(); 
+
+    println!("{}", total);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
