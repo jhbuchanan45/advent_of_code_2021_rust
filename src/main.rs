cd694d0747022ccc06a795ab837bd5bee8dd6eb0
Jacob Buchanan <jhabuchanan522@gmail.com>
2022-07-04 23:40:50 +0100

    day_9_part_1

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,65 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let mut prev = std::i32::MAX;
-    let mut increase_count = 0;
+    // using y, x indexing
+    let mut heightmap: Vec<Vec<u8>> = Vec::new();
 
     if let Ok(lines) = read_lines("input.txt") {
         for line in lines {
-            let cur = line.unwrap().parse::<i32>().unwrap();
-            if prev < cur {
-                increase_count += 1;
-            }
-            prev = cur;
+            let cur = line
+                .unwrap()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as u8)
+                .collect::<Vec<u8>>();
+
+            heightmap.push(cur);
         }
     }
 
-    println!("{}", increase_count);
+    // find and score low points
+    let y_max = heightmap.len() - 1;
+    let x_max = heightmap[0].len() - 1;
+    let mut total_score = 0;
+
+    for (y, row) in heightmap.iter().enumerate() {
+        for (x, height) in row.iter().enumerate() {
+            let mut lowest = true;
+
+            // top if not in row 0
+            if y != 0 {
+                if heightmap[y - 1][x] <= *height {
+                    lowest = false;
+                }
+            }
+
+            // left if not on column 0
+            if x != 0 {
+                if row[x - 1] <= *height {
+                    lowest = false;
+                }
+            }
+
+            // right if not in column max
+            if x != x_max {
+                if row[x + 1] <= *height {
+                    lowest = false;
+                }
+            }
+
+            // down if not bottom row
+            if y != y_max {
+                if heightmap[y + 1][x] <= *height {
+                    lowest = false;
+                }
+            }
+
+            if lowest {
+                total_score += 1 + *height as u32;
+            }
+        }
+    }
+
+    println!("{}", total_score);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
