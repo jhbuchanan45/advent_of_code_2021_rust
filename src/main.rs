d50e6e48b5cd16667fc74eaaee949080d6fef70b
Jacob Buchanan <jhabuchanan522@gmail.com>
2022-07-06 02:40:32 +0100

    day_9_part_2

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+struct Node {
+    y: usize,
+    x: usize,
+}
+
 fn main() {
     // using y, x indexing
     let mut heightmap: Vec<Vec<u8>> = Vec::new();
@@ -18,50 +23,91 @@ fn main() {
         }
     }
 
-    // find and score low points
-    let y_max = heightmap.len() - 1;
-    let x_max = heightmap[0].len() - 1;
-    let mut total_score = 0;
+    // transform grid into true/false map
+    let mut grid = heightmap
+        .iter()
+        .map(|row| row.iter().map(|h| h == &9).collect::<Vec<bool>>())
+        .collect::<Vec<Vec<bool>>>();
 
-    for (y, row) in heightmap.iter().enumerate() {
-        for (x, height) in row.iter().enumerate() {
-            let mut lowest = true;
+    // count each basin
+    let mut basin_sizes = Vec::new();
+    let mut last_basin = Node { y: 0, x: 0 };
 
-            // top if not in row 0
-            if y != 0 {
-                if heightmap[y - 1][x] <= *height {
-                    lowest = false;
-                }
-            }
-
-            // left if not on column 0
-            if x != 0 {
-                if row[x - 1] <= *height {
-                    lowest = false;
-                }
-            }
-
-            // right if not in column max
-            if x != x_max {
-                if row[x + 1] <= *height {
-                    lowest = false;
-                }
-            }
-
-            // down if not bottom row
-            if y != y_max {
-                if heightmap[y + 1][x] <= *height {
-                    lowest = false;
-                }
-            }
-
-            if lowest {
-                total_score += 1 + *height as u32;
+    for y in (last_basin.y)..grid.len() {
+        for x in 0..grid[0].len() {
+            if !grid[y][x] {
+                // find and store size of next basin
+                last_basin = Node { y: y, x: x };
+                basin_sizes.push(dfs(&mut grid, &last_basin))
             }
         }
     }
 
-    println!("{}", total_score);
+    basin_sizes.sort();
+    basin_sizes.reverse();
+
+    println!("{}", basin_sizes[0] * basin_sizes[1] * basin_sizes[2]);
+}
+
+fn dfs(grid: &mut Vec<Vec<bool>>, node: &Node) -> u32 {
+    let mut count = 0;
+
+    grid[node.y][node.x] = true;
+    count += 1;
+
+    // top if not in row 0
+    if node.y != 0 {
+        if !grid[node.y - 1][node.x] {
+            count += dfs(
+                grid,
+                &Node {
+                    y: node.y - 1,
+                    x: node.x,
+                },
+            )
+        }
+    }
+
+    // left if not on column 0
+    if node.x != 0 {
+        if !grid[node.y][node.x - 1] {
+            count += dfs(
+                grid,
+                &Node {
+                    y: node.y,
+                    x: node.x - 1,
+                },
+            )
+        }
+    }
+
+    // right if not in column max
+    if node.x != grid[0].len() - 1 {
+        if !grid[node.y][node.x + 1] {
+            count += dfs(
+                grid,
+                &Node {
+                    y: node.y,
+                    x: node.x + 1,
+                },
+            )
+        }
+    }
+
+    // down if not bottom row
+    if node.y != grid.len() - 1 {
+        if !grid[node.y + 1][node.x] {
+            count += dfs(
+                grid,
+                &Node {
+                    y: node.y + 1,
+                    x: node.x,
+                },
+            )
+        }
+    }
+
+    return count;
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
