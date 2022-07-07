72d86a0f805ce9bc0f90e4eb2b9c3ae47f53df86
Jacob Buchanan <jhabuchanan522@gmail.com>
2022-07-07 17:43:26 +0100

    day_10_part_2

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,67 @@
+use core::str::Chars;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use core::str::Chars;
+
+const BRACKETS: ([char; 4], [char; 4]) = (['(', '[', '{', '<'], [')', ']', '}', '>']);
+
+struct BracketResult {
+    corrupt_char: Option<char>,
+    unclosed_brackets: Vec<usize>,
+}
 
 fn main() {
-    let mut corrupt_closers = Vec::new();
-    let mut clean_lines = Vec::new();
+    let mut unclosed_brackets = Vec::new();
 
     if let Ok(lines) = read_lines("input.txt") {
         for line in lines {
             let line = line.unwrap();
 
-            if let Some(corrupt_ch) = corruption_check(&line.chars()) {
-                corrupt_closers.push(corrupt_ch);
-            } else {
-                clean_lines.push(line);
+            let mut bracket_result = corruption_check(&line.chars());
+
+            if let Some(_corrupt_ch) = bracket_result.corrupt_char {
                 continue;
+            } else {
+                bracket_result.unclosed_brackets.reverse();
+                unclosed_brackets.push(bracket_result.unclosed_brackets);
             }
         }
     }
 
-    let score: u32 = corrupt_closers.iter().map(|ch| match ch {
-        ')' => 3,
-        ']' => 57,
-        '}' => 1197,
-        '>' => 25137,
-        _ => 0
-    }).sum();
+    let mut score = unclosed_brackets
+        .iter()
+        .map(|v| v.iter().fold(0, |acc, &ch| (acc * 5) + ch + 1))
+        .collect::<Vec<usize>>();
 
-    println!("{}", score);
+    score.sort();
+
+    println!("{}", score.get(score.len() / 2).unwrap());
 }
 
-fn corruption_check(line: &Chars) -> Option<char> {
-    let brackets = (['(', '{', '[', '<'], [')', '}', ']', '>']);
+fn corruption_check(line: &Chars) -> BracketResult {
     let mut open_stack = Vec::new();
 
     for ch in line.clone() {
-        if brackets.0.contains(&ch) {
-            open_stack.push(brackets.0.iter().position(|v| v == &ch).unwrap());
+        if BRACKETS.0.contains(&ch) {
+            open_stack.push(BRACKETS.0.iter().position(|v| v == &ch).unwrap());
         } else {
             let expected_index = open_stack.pop().unwrap();
 
-            if expected_index == brackets.1.iter().position(|v| v == &ch).unwrap() {
+            if expected_index == BRACKETS.1.iter().position(|v| v == &ch).unwrap() {
                 continue;
             } else {
-                return Option::Some(ch);
+                return BracketResult {
+                    corrupt_char: Option::Some(ch),
+                    unclosed_brackets: open_stack,
+                };
             }
         }
     }
 
-    return Option::None;
+    return BracketResult {
+        corrupt_char: Option::None,
+        unclosed_brackets: open_stack,
+    };
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
