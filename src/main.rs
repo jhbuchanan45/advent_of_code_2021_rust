c50fb3d5e277e0759bd0e326b8cc70e66a564346
Jacob Buchanan <jhabuchanan522@gmail.com>
2022-07-09 16:55:36 +0100

    update_blank

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,14 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let mut prev = std::i32::MAX;
-    let mut increase_count = 0;
 
     if let Ok(lines) = read_lines("input.txt") {
         for line in lines {
-            let cur = line.unwrap().parse::<i32>().unwrap();
-            if prev < cur {
-                increase_count += 1;
-            }
-            prev = cur;
+            let cur = line.unwrap();
         }
     }
 
-    println!("{}", increase_count);
+    println!("{}", 10);
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
